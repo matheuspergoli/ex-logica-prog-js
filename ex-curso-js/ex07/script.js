a46ad7a56663045e09bc8b99be2441b358ebd04b
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-03-24 12:12:05 -0300

    ex07

---
+++ ex-curso-js/ex07/script.js
@@ -0,0 +1,14 @@
+let idadeUm = 16
+let idadeDois = 20
+
+if (idadeUm >= 18) {
+    console.log('Pode entrar')
+} else {
+    console.log('Não pode entrar')
+}
+
+if (idadeDois >= 18) {
+    console.log('Pode entrar')
+} else {
+    console.log('Não pode entrar')
+}
