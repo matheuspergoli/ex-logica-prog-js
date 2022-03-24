52608ac0f71f30b32e014dca321e093502ba10a6
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-03-24 17:03:03 -0300

    ex20

---
+++ ex-curso-js/ex20/script.js
@@ -0,0 +1,9 @@
+function pessoa(idade) {
+    if (idade >= 18) {
+        console.log('Pode entrar na auto-escola')
+    } else {
+        console.log('Não pode entrar na auto-escola')
+    }
+}
+
+pessoa(22)
