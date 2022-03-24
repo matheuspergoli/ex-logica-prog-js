83d9474095a2415d28d6fbcde46d83825dc78c83
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-03-24 10:01:11 -0300

    ex04

---
+++ ex-curso-js/ex04/script.js
@@ -0,0 +1,5 @@
+console.log(5 >= 5 && 3 > 5)
+console.log(55 <= 100 && 35 <= 70)
+console.log(55 > 100 || 100 < 500)
+console.log((155 * 5) + 50 > 100 && 350 / 2 > 10)
+console.log(!(55 * 5) >= 50 || !70 > 20)
