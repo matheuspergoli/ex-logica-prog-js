1f370bdcc65dba8fc20dbb7ba7aa993e9a174f08
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-03-24 12:08:06 -0300

    ex06

---
+++ ex-curso-js/ex06/script.js
@@ -0,0 +1,8 @@
+const verdadeiro = true
+const falso = false
+const texto = 'Matheus Pergoli'
+const numero = 1999
+console.log(verdadeiro, typeof verdadeiro)
+console.log(falso, typeof falso)
+console.log(texto, typeof texto)
+console.log(numero, typeof numero)
