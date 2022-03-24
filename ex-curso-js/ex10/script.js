83e501bac0cd2ddea2c29f554bb9664ebbe4ec59
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-03-24 12:32:26 -0300

    ex10

---
+++ ex-curso-js/ex10/script.js
@@ -0,0 +1,7 @@
+const velocidade = 80
+
+if (velocidade > 80) {
+    console.log('Velocidade limite ultrapassada. MULTADO!')
+} else {
+    console.log('Velocidade permitida, dirija com cuidado.')
+}
