89732c9ca0740e3ab55d43a984636d1962e3cba9
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-03-25 16:41:44 -0300

    ex30

---
+++ ex-curso-js/ex30/script.js
@@ -0,0 +1,7 @@
+const nomes = ['Matheus', 'Karla', 'Arthur', 'Sophia', 'Alex']
+
+if (nomes.includes('Matheus')) {
+    console.log('Matheus existe')
+} else {
+    console.log('Matheus não existe')
+}
