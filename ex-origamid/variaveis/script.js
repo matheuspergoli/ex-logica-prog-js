75c3179171d89990580692a6d87da72b712cf2f6
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-04-20 18:46:01 -0300

    EX - Variáveis

---
+++ ex-origamid/variaveis/script.js
@@ -0,0 +1,15 @@
+// Declarar uma variável com o seu nome
+const meuNome = 'Matheus Pergoli'
+
+// Declarar uma variável com a sua idade
+const minhaIdade = 22
+
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+let minhaComidaFavorita
+
+// Atribuir valor a sua comida favorita
+minhaComidaFavorita = 'Lentilha'
+
+// Declarar 5 variáveis diferentes sem valores
+let nome, sobrenome, idade, cidade, sexo
