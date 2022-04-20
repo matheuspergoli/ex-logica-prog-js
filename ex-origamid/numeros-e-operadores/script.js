6488517fa92405f6e27179125ea7488a2097a701
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-04-20 19:01:10 -0300

    EX - Números e Operadores

---
+++ ex-origamid/numeros-e-operadores/script.js
@@ -0,0 +1,22 @@
+// Qual o resultado da seguinte expressão?
+var total = 10 + 5 * 2 / 2 + 20;
+// total = 35
+
+// Crie duas expressões que retornem NaN
+const invalidoUm = 'Número 10' * 10
+const invalidoDois = 'Outro número 10' * 20
+
+// Somar a string '200' com o número 50 e retornar 250
+const numeroEmString = '200'
+const numeroTeste = 50
+console.log(+numeroEmString + numeroTeste)
+
+// Incremente o número 5 e retorne o seu valor incrementado
+
+// Como dividir o peso por 2?
+var numero = '80';
+var unidade = 'kg';
+var peso = numero + unidade; // '80kg'
+var pesoPorDois = peso / 2; // NaN (Not a Number)
+pesoPorDois = numero / 2
+console.log(pesoPorDois)
