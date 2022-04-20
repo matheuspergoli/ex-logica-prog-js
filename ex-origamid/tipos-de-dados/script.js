12a049a4a44b437208b3c71fb4aba8e6f738131c
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-04-20 18:51:41 -0300

    EX - Tipos de Dados

---
+++ ex-origamid/tipos-de-dados/script.js
@@ -0,0 +1,20 @@
+// Declare uma variável contendo uma string
+const minhaString = 'Isso é uma String'
+
+// Declare uma variável contendo um número dentro de uma string
+const numeroDentroDeString = '3000'
+
+// Declare uma variável com a sua idade
+const minhaIdade = 22
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+const meuNome = 'Matheus'
+const meuSobrenome = 'Pergoli'
+console.log(meuNome + ' ' + meuSobrenome)
+
+// Coloque a seguinte frase em uma variável: It's time
+const fraseTeste = "It's time"
+
+// Verifique o tipo da variável que contém o seu nome
+console.log(typeof meuNome)
