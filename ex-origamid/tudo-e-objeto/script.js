af68845bf15aafc33493c3e16e41798f2f274aaa
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-04-21 18:08:38 -0300

    EX - Tudo é Objeto

---
+++ ex-origamid/tudo-e-objeto/script.js
@@ -0,0 +1,16 @@
+// nomeie 3 propriedades ou métodos de strings
+// replace
+// substring
+// length
+// toUpperCase
+// toLowerCase
+// trim
+
+// nomeie 5 propriedades ou métodos de elementos do DOM
+// createElement
+// getElementById
+// getElementsByClassName
+// getElementsByTagName
+// getElementsByName
+// querySelector
+// querySelectorAll
